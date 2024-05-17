eae834761ffe2475993ba438024593c0595ec428
Garrett Kinman <garrettkinman@gmail.com>
2024-05-17 14:49:32 -0400

    add posit types

--- src/softposit/types.nim
+++ src/softposit/types.nim
@@ -3,3 +3,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
+type
+    Posit8* = distinct uint8
+    Posit16* = distinct uint16
+    Posit32* = distinct uint32
